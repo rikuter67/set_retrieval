d6c449ed4c08ee2d3247e82106d5edd205446abf
rikuter67 <rikuto.01.05.04@gmail.com>
2024-10-22 14:57:00 +0900

    score

--- run.py
+++ run.py
@@ -35,11 +35,12 @@ def parser_run(): # parser for run.py
     parser.add_argument('-epochs', type=int, default=100, help='number of epochs, default=100')
     parser.add_argument('-lr', type=float, default=0.001, help='learning rate, default=0.001')
     parser.add_argument('-patience', type=int, default=5, help='early stopping patience, default=5')
-    parser.add_argument('-batch_size', type=int, default=20, help='batch size, default=50')
+    parser.add_argument('-batch_size', type=int, default=50, help='batch size, default=50')
     parser.add_argument('-rep_vec_num', type=int, default=41, help='number of representative vectors, default=41')
     parser.add_argument('-is_neg_down_sample', type=bool, default=True, help='negative down sampling, default=True')
     parser.add_argument('-pretrained_mlp', type=int, default=0, help='Whether pretrain MLP (not use FC_projection)')
     parser.add_argument('-mlp_projection_dim', type=int, default=128, help='MLP will be learned to achieve designated dimention')
+    parser.add_argument('-train', type=bool, default=True, help='Whether train the model')
     return parser
 #---------------------------------------------------------------------------------------
 
@@ -60,7 +61,7 @@ def parser_comp(): # parser for comp_results.py
 parser = parser_run()
 args = parser.parse_args()
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0' # choose GPU
+os.environ['CUDA_VISIBLE_DEVICES'] = '1' # choose GPU
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
@@ -146,18 +147,63 @@ if args.pretrained_mlp:
         model_mlp.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'], run_eagerly=True)
         val_loss, val_acc = model_mlp.evaluate(x_valid,y_valid)
 
-# train generator is to be switched to set_retrieval mode  
-#########################################　削除部分　##########################################
-train_generator = data.trainDataGenerator(year=args.year, batch_size=args.batch_size, max_item_num=args.max_item_num)
-x_valid, x_size_valid, y_valid = train_generator.data_generation_val()
-#########################################　削除部分　##########################################
+
+    # train generator is to be switched to set_retrieval mode  
+    #########################################　削除部分　##########################################
+    train_generator = data.trainDataGenerator(year=args.year, batch_size=args.batch_size, max_item_num=args.max_item_num)
+    x_valid, x_size_valid, y_valid = train_generator.data_generation_val()
+    #########################################　削除部分　##########################################
+
+else:
+    # train generator is to be switched to set_retrieval mode  
+    #########################################　削除部分　##########################################
+    train_generator = data.trainDataGenerator(year=args.year, batch_size=args.batch_size, max_item_num=args.max_item_num)
+    x_valid, x_size_valid, y_valid = train_generator.data_generation_val()
+    #########################################　削除部分　##########################################
+
+
 
 #---------------------------------------------------------------------------------------
+
+
+# # SetMatchingModelの定義と学習
+set_matching_model = models.SetMatchingModel(
+    isCNN=False,                     # CNNを使用するかどうか
+    is_TrainableMLP=args.pretrained_mlp, # MLPを学習するかどうか（デフォルト: False）
+    is_set_norm=args.is_set_norm,    # Set Normalizationの有無
+    is_cross_norm=args.is_cross_norm,# Cross Normalizationの有無
+    is_final_linear=True,            # 最終的な線形層を使用するかどうか（デフォルト: True）
+    num_layers=args.num_layers,      # エンコーダ・デコーダのレイヤー数
+    num_heads=args.num_heads,        # Attentionのヘッド数
+    mode='setRepVec_pivot',          # モード（デフォルト: 'setRepVec_pivot'）
+    baseChn=64,            # ベースのチャンネル数
+    baseMlp=args.mlp_projection_dim * 4,              # ベースのMLPのチャンネル数
+    rep_vec_num=1,                   # 代表ベクトルの数
+    seed_init = seed_vectors,                   # シードの初期化
+    cnn_class_num=2,                 # CNNの分類クラス数（デフォルト: 2）
+    max_channel_ratio=2,             # チャンネル倍率
+    is_neg_down_sample=args.is_neg_down_sample # ネガティブサンプルのダウンサンプリングを使用するかどうか
+)
+
+# 学習
+SetMatching_model_path = f"{experimentPath}_TrainScore"
+cp_callback = tf.keras.callbacks.ModelCheckpoint(SetMatching_model_path, monitor='val_Set_accuracy', save_weights_only=True, mode='max', save_best_only=True, save_freq='epoch', verbose=1)
+cp_earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_Set_accuracy', patience=args.patience, mode='max', min_delta=0.001, verbose=1)
+
+set_matching_model.compile(optimizer='adam', loss=util.Set_hinge_loss, metrics=util.Set_accuracy)
+set_matching_model.fit(train_generator, epochs=args.epochs, validation_data=((x_valid, x_size_valid), y_valid), callbacks=[cp_callback, cp_earlystopping])
+
+# 事前学習モデルの保存
+set_matching_model.save_weights('experiment_TrainScore/set_matching_weights.ckpt')
+#---------------------------------------------------------------------------------------
+
+
+
 # set-matching network
 if args.model == 'VLAD':
     model = models.VLAD(num_features=args.baseChn, k_centers=args.rep_vec_num, batch_size=args.batch_size, seed_vectors = seed_vectors)
 elif args.model == 'SMN':
-    model = models.SMN(isCNN=False, is_TrainableMLP=False, is_set_norm=args.is_set_norm, is_cross_norm=args.is_cross_norm, num_layers=args.num_layers, \
+    model = models.SMN(isCNN=False, is_TrainableMLP=args.pretrained_mlp, is_set_norm=args.is_set_norm, is_cross_norm=args.is_cross_norm, num_layers=args.num_layers, \
                            num_heads=args.num_heads, baseChn=args.baseChn, baseMlp = baseMLPChn, mode=args.mode, calc_set_sim=args.calc_set_sim, rep_vec_num=args.rep_vec_num, seed_init = seed_vectors, is_neg_down_sample=args.is_neg_down_sample, use_Cvec = args.use_Cvec, is_Cvec_linear=args.is_Cvec_linear)
 elif args.model == 'random':
     model = models.random()
@@ -167,10 +213,13 @@ elif args.model == 'CNN':
 # model.build([(100, 5, 4096), (100,)])
 # model.summary()
 
-main_path = os.path.join(main_model_path)
-main_checkpoint_path = os.path.join(main_path,"model/cp.ckpt")
-main_checkpoint_dir = os.path.dirname(main_checkpoint_path)
-main_result_path = os.path.join(main_path,"result/result.pkl")
+if not os.path.exists(main_model_path):
+    os.makedirs(main_model_path)
+    os.makedirs(os.path.join(main_model_path, 'model'))
+    os.makedirs(os.path.join(main_model_path, 'result'))
+
+main_checkpoint_path = os.path.join(main_model_path,"model/cp.ckpt")
+main_result_path = os.path.join(main_model_path,"result/result.pkl")
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
@@ -178,12 +227,14 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(main_checkpoint_path, monitor='
 cp_earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_Set_accuracy', patience=args.patience, mode='max', min_delta=0.001, verbose=1)
 
 #########################################　追加部分　##########################################
+
 # train_generator = data.DataGenerator(year=args.year, batch_size=args.batch_size, max_item_num=args.max_item_num)
 
 # train_data_set = train_generator.get_train_dataset()
 # validation_data_set = train_generator.get_validation_dataset()
-# train_data_set = train_data_set.prefetch(tf.data.AUTOTUNE)
-# validation_data_set = validation_data_set.prefetch(tf.data.AUTOTUNE)
+# train_data_set = train_data_set.prefetch(1)
+# validation_data_set = validation_data_set.prefetch(1)
+
 #########################################　追加部分　##########################################
 
 # for batch in train_data_set.take(1):
@@ -198,16 +249,28 @@ cp_earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_Set_accuracy', 
 # else:
 #     initial_epoch = 0
 
-
 #################################################################################################
-if not os.path.exists(main_result_path):
-
+# if not os.path.exists(main_result_path): # if mode is train とかに変える
+if args.train:
     # setting training, loss, metric to model
     model.compile(optimizer="adam", loss=util.Set_hinge_loss, metrics=util.Set_accuracy, run_eagerly=True)
+    
+    # train_steps, validation_steps = train_generator.__len__()
+
     # execute training
+    # history = model.fit(
+    #     train_generator, 
+    #     steps_per_epoch=train_steps, 
+    #     epochs=args.epochs, 
+    #     validation_data=validation_data_set, 
+    #     validation_steps=validation_steps,
+    #     shuffle=True, 
+    #     callbacks=[cp_callback,cp_earlystopping],
+    #     verbose=1
+    #     ) # 吉田法
+    
     # history = model.fit(x=(x_train, x_size_train), y=y_train, validation_data=((x_valid, x_size_valid), y_valid), epochs=args.epochs, shuffle=True, callbacks=[cp_callback, cp_earlystopping])
-    # history = model.fit(train_generator, epochs=args.epochs, validation_data=((x_valid, x_size_valid), y_valid), shuffle=True, callbacks=[cp_callback,cp_earlystopping])
-    history = model.fit(train_generator, epochs=args.epochs, validation_data=((x_valid, x_size_valid), y_valid), shuffle=True, callbacks=[cp_callback,cp_earlystopping])
+    history = model.fit(train_generator, epochs=args.epochs, validation_data=((x_valid, x_size_valid), y_valid), shuffle=True, callbacks=[cp_callback,cp_earlystopping]) #山園法
     # model.evaluate((x_valid, x_size_valid), y_valid)
     # loss, accuracy = model.evaluate((x_valid, x_size_valid), y_valid)
     # print(f"Validation loss: {loss}, Validation accuracy: {accuracy}")
@@ -218,10 +281,10 @@ if not os.path.exists(main_result_path):
     val_loss = history.history['val_loss']
 
     # plot loss & acc
-    util.plotLossACC(main_path,loss,val_loss,acc,val_acc)
+    util.plotLossACC(main_model_path,loss,val_loss,acc,val_acc)
 
     # dump to pickle
-    with open(main_model_path,'wb') as fp:
+    with open(main_result_path,'wb') as fp:
         pickle.dump(acc,fp)
         pickle.dump(val_acc,fp)
         pickle.dump(loss,fp)
@@ -231,4 +294,8 @@ else:
     # load trained parameters
     print("load models")
     model.load_weights(main_checkpoint_path)
+
+# モデルパラメータの保存
+model.save_weights(main_checkpoint_path)
+
 ################################################################################################
